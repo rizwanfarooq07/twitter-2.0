89739e7fac4ba9b8d0cedc72db1d57ca5d295a6c
rizwanfarooq07 <rizwanfarooqui07.com>
2022-06-29 12:26:54 +0530

    final commit

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,7 +1,7 @@
 import { Tweet } from "../typings";
 
 export const fetchTweets = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getTweets`);
+  const res = await fetch(`https://twitter-2-0-seven.vercel.app/api/getTweets`);
   const data = await res.json();
   const Tweets: Tweet[] = data.tweets;
   return Tweets;
